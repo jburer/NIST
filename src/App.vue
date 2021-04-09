ab8729e4ca98f7829c25a7ae67b20c43e093df74
jburer <jburer@gmail.com>
2021-04-09 16:41:46 -0400

    Initial Vuetify Settings

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
-  <div id="app">
-    <NavBar />
-    <router-view :key="$route.fullPath" />
-  </div>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col>
+            <NavBar />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col class="mx-12">
+            <router-view :key="$route.fullPath" />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
 import NavBar from "@/components/NavBar.vue";
 
 export default {
+  name: "App",
   components: {
     NavBar
   },
@@ -22,44 +35,3 @@ export default {
   }
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 15px;
-}
-
-#nav a:link {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a:visited {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a:hover {
-  font-weight: bold;
-  color: #2c3e50;
-  text-decoration: underline;
-}
-
-#nav a:active {
-  font-weight: bold;
-  color: #2c3e50;
-  text-decoration: underline;
-}
-/*
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-*/
-</style>
